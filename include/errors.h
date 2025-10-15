631a3a22e5146c7e00c7215d93c82a1919c00588
kayibea <kayibea@proton.me>
2025-10-16 01:55:19 +0200

    feat: add ERR_OUT_OF_RANGE macro for formating error messages

--- include/errors.h
+++ include/errors.h
@@ -8,6 +8,10 @@
 #define ERR_INVALID_HEX_STRING "Invalid hex string"
 #define ERR_OFFSET_OUT_OF_RANGE "Offset out of range"
 
+#define ERR_OUT_OF_RANGE                                                     \
+  "The value of \"%s\" is out of range. It must be >= 0 && <= %I. Received " \
+  "\"%I\""
+
 #define ERR_UNSUPPORTED_ENCODING \
   "Unsupported encoding: \"%s\" (supported: " SUPPORTED_ENCODINGS ")"
 
